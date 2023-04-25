91098973b0d60ebff354fb5de1f190e34e92754d
Tracy Homer <thomer@mac.com>
2023-04-25 19:48:25 -0400

    Bump version: 0.0.16 → 0.0.17

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/TJHomer/spatialcsv',
-    version='0.0.16',
+    version='0.0.17',
     zip_safe=False,
 )
